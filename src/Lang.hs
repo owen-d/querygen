f3aeec0bdd885b4c5393b113170f902ef0824666
Owen Diehl <ow.diehl@gmail.com>
2020-07-01 20:43:05 -0400

    adding show impls Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- src/Lang.hs
+++ src/Lang.hs
@@ -1,22 +1,97 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lang
         ()
 where
 
+import           Data.List                      ( intercalate )
 -- | MatchType is a matcher
 data MatchType = MatchEquals | MatchNotEquals | MatchRegexp | MatchNotRegexp
+
+instance Show MatchType where
+        show x = case x of
+                MatchEquals    -> "="
+                MatchNotEquals -> "!="
+                MatchRegexp    -> "=~"
+                MatchNotRegexp -> "!~"
 
 -- | Selector is selection
 data Selector = Selector String MatchType String
 
-data Operator = Add | Div | Eql | EqlRegex | Gte | Gtr | And | Or | Less | Lte | LUnless | Mod | Mul | Neq | NeqRegex | Pow | Sub
+instance Show Selector where
+        show (Selector label mt matcher) = concat
+                [quote, label, quote, (show mt), quote, matcher, quote]
+                where quote = "\""
 
-data Aggregator = Avg | Bottomk Int | Count | CountValues | Max | Min | Quantile Float | Stddev | Stdvar | Sum | Topk Int
+data Operator = Add | Div | Eql | Gte | Gtr | And | Or | Less | Lte | Unless | Mod | Mul | Neq | Pow | Sub
 
-type Scalar = Either String Float
+instance Show Operator where
+        show x = case x of
+                Add    -> "+"
+                Div    -> "/"
+                Eql    -> "=="
+                Gte    -> ">="
+                Gtr    -> ">"
+                And    -> "and"
+                Or     -> "or"
+                Less   -> "<"
+                Lte    -> "<="
+                Unless -> "unless"
+                Mod    -> "%"
+                Mul    -> "*"
+                Neq    -> "!="
+                Pow    -> "^"
+                Sub    -> "-"
+
+data Aggregator = Avg | Count | CountValues | Max | Min  | Stddev | Stdvar | Sum | TwoArityAggregator TwoArityAggregator
+
+instance Show Aggregator where
+        show x = case x of
+                Avg                  -> "avg"
+                Count                -> "count"
+                CountValues          -> "count_values"
+                Max                  -> "max"
+                Min                  -> "min"
+                Stddev               -> "stdev"
+                Stdvar               -> "stdvar"
+                Sum                  -> "sum"
+                TwoArityAggregator x -> show x
+
+data TwoArityAggregator = Bottomk Int | Quantile Float | Topk Int
+
+instance Show TwoArityAggregator where
+        show x = case x of
+                Bottomk  v -> "bottomk"
+                Topk     _ -> "topk"
+                Quantile _ -> "quantile_over_time"
+
+data Scalar = Scalar (Either String Float)
+
+instance Show Scalar where
+        show (Scalar x) = case x of
+                Left  v -> v
+                Right v -> show v
 
 data Metric = Metric String [Selector]
 
-data InstantVector = Scalar Scalar | Operator Operator InstantVector InstantVector | Aggregator Aggregator InstantVector | VecFn Function
+instance Show Metric where
+        show (Metric x []) = x
+        show (Metric x ys) = concat [x, "{", labels, "}"]
+                where labels = intercalate "," $ map show ys
+
+data InstantVector = Operator Operator InstantVector InstantVector | Aggregator Aggregator InstantVector | VecFn Function
+
+instance Show InstantVector where
+        show (Operator op vec1 vec2) = concat [show vec1, show op, show vec2]
+        show (Aggregator agg vec   ) = case agg of
+                TwoArityAggregator x -> concat
+                        [show x, "(", v, ",", show vec, ")"]
+                    where
+                        v = case x of
+                                Bottomk  v -> show v
+                                Topk     v -> show v
+                                Quantile v -> show v
+                y -> concat [show y, "(", show vec, ")"]
+        show _ = ""
 
 data Function = Vector Scalar | Round InstantVector (Maybe Int) | Sqrt InstantVector | Floor InstantVector | Ceil InstantVector | Rate RangeVector | Ln InstantVector | Increase RangeVector
 
